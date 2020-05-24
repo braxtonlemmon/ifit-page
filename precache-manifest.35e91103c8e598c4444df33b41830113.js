f7a2a8a931a9a9e26eee618bd5e677affc07f71e
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-05-24 15:36:28 +0200

    Updates

---
+++ precache-manifest.35e91103c8e598c4444df33b41830113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8481edf519a92c36045f48628c798165",
+    "url": "/ifit-page/index.html"
+  },
+  {
+    "revision": "5c56d6eb2ba61ae02e56",
+    "url": "/ifit-page/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c74266e2f71b72f307b",
+    "url": "/ifit-page/static/js/2.ba7fdda2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/ifit-page/static/js/2.ba7fdda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c56d6eb2ba61ae02e56",
+    "url": "/ifit-page/static/js/main.c334cae1.chunk.js"
+  },
+  {
+    "revision": "1a3630cc0f23ae036e3c",
+    "url": "/ifit-page/static/js/runtime-main.7f490996.js"
+  }
+]);
